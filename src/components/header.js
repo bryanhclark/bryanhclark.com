343c4cdf0de1f5be25da2d2975e1d98f1a607166
bryan clark <bryan@leighs-MBP.home>
2018-05-01 15:18:10 -0400

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default ()=> (
+    <div className="vh-100 dt w-100 cover homeContainer">
+        <header className="sans-serif tc ph3">
+            <h1 className="f2 f1-l fw2 black-90 mb0 lh-title">Bryan Clark</h1>
+            <p className="f5 f4-l db fw1 ma1"> Software Engineer</p>
+        </header>
+    </div>
+)
