037626d786d4d9df46b87cd7227fcaa7ed284e6c
bryan clark <bryan@leighs-MBP.home>
2018-05-01 18:11:44 -0400

    about page template done

--- src/components/about.js
+++ src/components/about.js
@@ -3,9 +3,19 @@ import React from 'react'
 
 
 export default ()=> (
-    <div className="aboutContainer br2 bg-washed-green mw7 mt4 center sans-serif pa1">
-        <h2 className="f2 f1-l fw2 black-90 mb0 mt2 pl2">About:</h2>
-        <p className="fw1">Laboris in labore esse exercitation. Amet mollit labore voluptate consequat mollit ex. Aute ut sint nisi labore id mollit irure ex sit Lorem commodo dolor aute. Ut incididunt est quis dolore ad ipsum deserunt ad nisi. Dolor do nisi ipsum officia consequat anim ex cupidatat qui incididunt deserunt quis ullamco non. Consectetur dolor proident reprehenderit sunt est magna.</p>
+    <div className="aboutMainContainer">
+        <div className="contentContainer br2 bg-washed-green mw7 mt4 center sans-serif pa1">
+            <h2 className="f2 f1-l fw2 black-90 mb0 mt2 pl2">About</h2>
+            <p className="fw1">Laboris in labore esse exercitation. Amet mollit labore voluptate consequat mollit ex. Aute ut sint nisi labore id mollit irure ex sit Lorem commodo dolor aute. Ut incididunt est quis dolore ad ipsum deserunt ad nisi. Dolor do nisi ipsum officia consequat anim ex cupidatat qui incididunt deserunt quis ullamco non. Consectetur dolor proident reprehenderit sunt est magna.</p>
+        </div>
+        <div className="contentContainer br2 bg-lightest-blue mw7 mt4 center sans-serif pa1">
+            <h2 className="f2 f1-l fw2 black-90 mb0 mt2 pr2 tr">Gemologist / Facetor</h2>
+            <p className="fw1">Laboris in labore esse exercitation. Amet mollit labore voluptate consequat mollit ex. Aute ut sint nisi labore id mollit irure ex sit Lorem commodo dolor aute. Ut incididunt est quis dolore ad ipsum deserunt ad nisi. Dolor do nisi ipsum officia consequat anim ex cupidatat qui incididunt deserunt quis ullamco non. Consectetur dolor proident reprehenderit sunt est magna.</p>
+        </div>
+        <div className="contentContainer br2 bg-washed-yellow mw7 mt4 center sans-serif pa1">
+            <h2 className="f2 f1-l fw2 black-90 mb0 mt2 pl2 tl">Drummer</h2>
+            <p className="fw1">Laboris in labore esse exercitation. Amet mollit labore voluptate consequat mollit ex. Aute ut sint nisi labore id mollit irure ex sit Lorem commodo dolor aute. Ut incididunt est quis dolore ad ipsum deserunt ad nisi. Dolor do nisi ipsum officia consequat anim ex cupidatat qui incididunt deserunt quis ullamco non. Consectetur dolor proident reprehenderit sunt est magna.</p>
+        </div>
     </div>
 )
 
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,9 +6,9 @@ export default () => (
     <nav className="dt w-100 mw8 center"> 
         <div className="v-mid tc">
             <ul className="h2">
-                <NavLink to='/'><li className="f6 fw4 hover-black no-underline black-70 dn dib pv2 ph3">Home</li></NavLink> 
-                <NavLink to='/about'><li className="f6 fw4 hover-black no-underline black-70 dn dib pv2 ph3">About</li></NavLink>  
-                <NavLink to='/portfolio'><li className="f6 fw4 hover-black no-underline black-70 dn dib pv2 ph3">Portfolio</li></NavLink> 
+                <NavLink exact to='/'><li className="f6 fw4 grow hover-black no-underline black-70 dn dib pv2 ph3">Home</li></NavLink> 
+                <NavLink to='/about'><li className="f6 fw4 grow hover-black no-underline black-70 dn dib pv2 ph3">About</li></NavLink>  
+                <NavLink to='/portfolio'><li className="f6 fw4 grow hover-black no-underline black-70 dn dib pv2 ph3">Portfolio</li></NavLink> 
             </ul> 
         </div>
       </nav>
