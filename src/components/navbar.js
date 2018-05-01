75492bfd565a7802924371a9e785dd5acbc0c8a2
bryan clark <bryan@leighs-MBP.home>
2018-05-01 15:37:40 -0400

    back to before

---
+++ src/components/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+
+export default () => (
+    <nav className="dt w-100 mw8 center"> 
+        <div className="v-mid tc pa3">
+          <a className="f6 fw4 hover-black no-underline black-70 dn dib pv2 ph3" href="/" >About</a> 
+          <a className="f6 fw4 hover-black no-underline black-70 dn dib pv2 ph3" href="/" >Portfolio</a> 
+          <a className="f6 fw4 hover-black no-underline black-70 dn dib pv2 ph3" href="/" >Personal</a> 
+        </div>
+      </nav>
+)
