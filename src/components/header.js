529d0b33de5ed8dcad13f85bf872745ceafa0f2a
bryan clark <bryan@leighs-MBP.home>
2018-05-01 15:28:59 -0400

    first tachyon commit

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
 export default ()=> (
-    <div className="vh-100 dt w-100 cover homeContainer">
-        <header className="sans-serif tc ph3">
+    <div className="vh-100 dt w-100 cover">
+        <header className="sans-serif light-red tc ph3">
             <h1 className="f2 f1-l fw2 black-90 mb0 lh-title">Bryan Clark</h1>
             <p className="f5 f4-l db fw1 ma1"> Software Engineer</p>
         </header>
