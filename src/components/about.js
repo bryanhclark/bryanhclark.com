7076f8f8527fa9054a1094ed98f0917a497f3efa
bryan clark <bryan@leighs-MBP.home>
2018-05-01 17:02:57 -0400

    about section template finished

--- src/components/about.js
+++ src/components/about.js
@@ -3,7 +3,9 @@ import React from 'react'
 
 
 export default ()=> (
-    <div className="aboutContainer">
-        <h1>About Component</h1>
+    <div className="aboutContainer br2 bg-washed-green mw7 mt4 center sans-serif pa1">
+        <h2 className="f2 f1-l fw2 black-90 mb0 mt2 pl2">About:</h2>
+        <p className="fw1">Laboris in labore esse exercitation. Amet mollit labore voluptate consequat mollit ex. Aute ut sint nisi labore id mollit irure ex sit Lorem commodo dolor aute. Ut incididunt est quis dolore ad ipsum deserunt ad nisi. Dolor do nisi ipsum officia consequat anim ex cupidatat qui incididunt deserunt quis ullamco non. Consectetur dolor proident reprehenderit sunt est magna.</p>
     </div>
 )
+
