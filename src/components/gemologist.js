0a7571d862ff1f66c8c998d18e6f26383c51ed23
bryan clark <bryanhclark@gmail.com>
2018-05-24 09:50:03 -0400

    added to gemologist page

--- src/components/gemologist.js
+++ src/components/gemologist.js
@@ -8,6 +8,8 @@ export default ()=> (
         <div className="contentContainer br2 bg-washed-yellow mw7 mt4 center sans-serif pa1 mb5">
             <h2 className="f2 f1-l fw2 black-90 mb0 mt2 pl2 tl">Gemologist</h2>
             <p className="fw1 ph2"></p>
+            <p className="fw1 ph2"></p>
+            <p className="fw1 ph2"></p>
         </div>
     </div>
 )
